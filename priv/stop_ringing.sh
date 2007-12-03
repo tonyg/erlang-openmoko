9b1575e04d60cd19beb5f958422c05370b4ded5c
Tony Garnock-Jones <tonyg@lshift.net>
2007-12-03 12:50:12 +0000

    [project @ Be crude until we figure out why pid recording doesn't work]

--- priv/stop_ringing.sh
+++ priv/stop_ringing.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
-kill `cat /tmp/ringtone-player-pid`
-rm -f /tmp/ringtone-player-pid
+
+# Do the crude thing until we figure out why ringtone-player-pid ends up empty:
+killall gst-launch-0.10
+#kill `cat /tmp/ringtone-player-pid`
+#rm -f /tmp/ringtone-player-pid
