efd724f10ebff07854874f51e5a82336d53ecfd6
Tony Garnock-Jones <tonyg@kcbbs.gen.nz>
2007-12-05 23:44:19 +0000

    [project @ Submit only one command at once, waiting for completion before starting the next.]

--- src/modem_server.erl
+++ src/modem_server.erl
@@ -64,7 +64,7 @@ setup_modem() ->
 %---------------------------------------------------------------------------
 %% Implementation
 
--record(state, {peer_state, config, port, unsent_commands, pending_commands, line_accumulator}).
+-record(state, {peer_state, config, port, unsent_commands, pending_command, line_accumulator}).
 -record(pending, {command, body_lines, from, lines}).
 
 mk_pending(CommandString, BodyLines, From) ->
@@ -73,14 +73,28 @@ mk_pending(CommandString, BodyLines, From) ->
 	     from = From,
 	     lines = []}.
 
-send_and_enqueue(CommandString, BodyLines, From, State) ->
-    send_and_enqueue(mk_pending(CommandString, BodyLines, From), State).
+submit(CommandString, BodyLines, From, State) ->
+    submit(mk_pending(CommandString, BodyLines, From), State).
 
-send_and_enqueue(Pending = #pending{command = CommandString,
-				    body_lines = BodyLines},
-		 State = #state{peer_state = ready,
-				port = Port,
-				pending_commands = OldKs}) ->
+submit(Pending, State = #state{unsent_commands = UnsentQ}) ->
+    submit_next(State#state{unsent_commands = queue:in(Pending, UnsentQ)}).
+
+submit_next(State = #state{peer_state = ready,
+			   port = Port,
+			   pending_command = none,
+			   unsent_commands = UnsentQ}) ->
+    case queue:out(UnsentQ) of
+	{{value, Pending}, Rest} ->
+	    ok = submit_one(Pending, Port),
+	    State#state{pending_command = Pending,
+			unsent_commands = Rest};
+	{empty, _} ->
+	    State
+    end;
+submit_next(State) ->
+    State.
+
+submit_one(#pending{command = CommandString, body_lines = BodyLines}, Port) ->
     error_logger:info_msg("Sending command ~p~n", [CommandString]),
     Port ! {send, CommandString ++ [13]},
     case BodyLines of
@@ -92,12 +106,7 @@ send_and_enqueue(Pending = #pending{command = CommandString,
 			  end, BodyLines),
 	    Port ! {send, [26]},
 	    ok
-    end,
-    State#state{pending_commands = queue:in(Pending, OldKs)};
-send_and_enqueue(Pending = #pending{command = CommandString},
-		 State = #state{unsent_commands = Unsent}) ->
-    error_logger:info_msg("Queueing command ~p~n", [CommandString]),
-    State#state{unsent_commands = queue:in(Pending, Unsent)}.
+    end.
 
 process_incoming("", State) ->
     State;
@@ -113,38 +122,28 @@ terminate_reply(State = #state{line_accumulator = Acc}) ->
 
 process_reply("", State) ->
     State;
-process_reply(Line, State = #state{pending_commands = OldKs}) ->
-    case queue:out(OldKs) of
-	{{value, Pending = #pending{command = CommandString, from = From, lines = Lines}}, Ks} ->
-	    case Line of
-		CommandString ->
-		    error_logger:info_msg("Ignoring echo: ~p~n", [Line]),
-		    State;
-		_ ->
-		    case analyse_response(Line) of
-			{final, Summary} ->
-			    send_reply(From, CommandString, {Summary, Line, lists:reverse(Lines)}),
-			    State#state{pending_commands = Ks};
-			partial ->
-			    error_logger:info_msg("Got partial reply ~p to command ~p~n",
-						  [Line, CommandString]),
-			    State#state{pending_commands =
-					  queue:in_r(Pending#pending{lines = [Line | Lines]},
-						     Ks)};
-			unsolicited ->
-			    unsolicited(Line, State)
-		    end
-	    end;
-	{empty, _} ->
-	    unsolicited(Line, State)
-    end.
-
-send_unsent(Q, State) ->
-    case queue:out(Q) of
-	{{value, Pending}, Rest} ->
-	    send_unsent(Rest, send_and_enqueue(Pending, State));
-	{empty, _} ->
-	    State
+process_reply(Line, State = #state{pending_command = none}) ->
+    unsolicited(Line, State);
+process_reply(Line, State = #state{pending_command =
+				   Pending = #pending{command = CommandString,
+						      from = From,
+						      lines = Lines}}) ->
+    case Line of
+	CommandString ->
+	    error_logger:info_msg("Ignoring echo: ~p~n", [Line]),
+	    State;
+	_ ->
+	    case analyse_response(Line) of
+		{final, Summary} ->
+		    send_reply(From, CommandString, {Summary, Line, lists:reverse(Lines)}),
+		    submit_next(State#state{pending_command = none});
+		partial ->
+		    error_logger:info_msg("Got partial reply ~p to command ~p~n",
+					  [Line, CommandString]),
+		    State#state{pending_command = Pending#pending{lines = [Line | Lines]}};
+		unsolicited ->
+		    unsolicited(Line, State)
+	    end
     end.
 
 parse_clip(ClipInfo) ->
@@ -152,18 +151,15 @@ parse_clip(ClipInfo) ->
     [QuotedPhoneNumber | Pieces] = lists:map(fun string:strip/1, AllPieces),
     [string:strip(QuotedPhoneNumber, both, $") | Pieces].
 
-unsolicited(Line, State = #state{peer_state = initialising,
-				 unsent_commands = Unsent}) ->
+unsolicited(Line, State = #state{peer_state = initialising}) ->
     case regexp:match(Line, "AT-Command Interpreter ready$" % " - emacs balancing
 		     ) of
 	nomatch ->
 	    error_logger:warning_msg("Swallowing junk during modem initialisation: ~p~n", [Line]),
 	    State;
 	{match, _, _} ->
-	    error_logger:info_msg("Modem ready.~n"),
 	    openmoko_event:notify(modem_ready),
-	    send_unsent(Unsent, State#state{peer_state = ready,
-					    unsent_commands = queue:new()})
+	    submit_next(State#state{peer_state = ready})
     end;
 unsolicited("RING", State) ->
     openmoko_event:notify(modem_ringing),
@@ -216,6 +212,11 @@ send_reply(From, CommandString, Reply) ->
 send_error(#pending{command = CommandString, from = From}, Reason) ->
     send_reply(From, CommandString, {error, Reason, unknown}).
 
+abort_pending(Reason, State = #state{pending_command = Pending, unsent_commands = UnsentQ}) ->
+    send_error(Pending, Reason),
+    lists:foreach(fun (P) -> send_error(P, Reason) end, queue:to_list(UnsentQ)),
+    State#state{pending_command = none, unsent_commands = queue:new()}.
+
 set_power_control_string(PowerControlString,
 			 #state{config = #config{modem_power_control_file
 						 = PowerControlFile}}) ->
@@ -225,13 +226,11 @@ set_power_control_string(PowerControlString,
 
 close_serial_port(State = #state{port = none}) ->
     State;
-close_serial_port(State = #state{port = Port,
-				 pending_commands = Ks}) ->
+close_serial_port(State = #state{port = Port}) ->
     Port ! stop,
-    lists:foreach(fun (P) -> send_error(P, port_closed) end, queue:to_list(Ks)),
-    State#state{peer_state = closed,
-		port = none,
-		pending_commands = queue:new()}.
+    abort_pending(port_closed,
+		  State#state{peer_state = closed,
+			      port = none}).
 
 open_serial_port(State = #state{config = #config{modem_module = ModemModule,
 						 modem_device = DeviceName},
@@ -263,7 +262,7 @@ init([Config]) ->
 		    config = Config,
 		    port = none,
 		    unsent_commands = queue:new(),
-		    pending_commands = queue:new(),
+		    pending_command = none,
 		    line_accumulator = ""},
     {ok, internal_powercycle(State0)}.
 
@@ -274,15 +273,15 @@ handle_call({set_power, on}, _From, State) ->
 handle_call(get_peer_state, _From, State = #state{peer_state = PeerState}) ->
     {reply, {ok, PeerState}, State};
 handle_call({cmd, CommandString, BodyLines}, From, State) ->
-    {noreply, send_and_enqueue(CommandString, BodyLines, From, State)};
+    {noreply, submit(CommandString, BodyLines, From, State)};
 handle_call(Request, _From, State) ->
     error_logger:error_msg("Unknown modem_server:handle_call ~p~n", [Request]),
     {noreply, State}.
 
 handle_cast({cmd, CommandString, BodyLines}, State) ->
-    {noreply, send_and_enqueue(CommandString, BodyLines, none, State)};
+    {noreply, submit(CommandString, BodyLines, none, State)};
 handle_cast({cmd, CommandString, BodyLines, AsyncReplyPid}, State) ->
-    {noreply, send_and_enqueue(CommandString, BodyLines, {async, AsyncReplyPid}, State)};
+    {noreply, submit(CommandString, BodyLines, {async, AsyncReplyPid}, State)};
 handle_cast(Message, State) ->
     error_logger:error_msg("Unknown modem_server:handle_cast ~p~n", [Message]),
     {noreply, State}.
@@ -302,9 +301,8 @@ handle_info(Message, State) ->
     error_logger:error_msg("Unknown modem_server:handle_info ~p~n", [Message]),
     {noreply, State}.
 
-terminate(Reason, #state{pending_commands = Ks}) ->
-    lists:foreach(fun (P) -> send_error(P, {modem_server_terminated, Reason}) end,
-		  queue:to_list(Ks)),
+terminate(Reason, State) ->
+    abort_pending({modem_server_terminated, Reason}, State),
     ok.
 
 code_change(_OldVsn, State, _Extra) ->
