d3be383d11f27b4bd7087e7b030b55f1eecaa71f
Tony Garnock-Jones <tonyg@lshift.net>
2007-12-07 00:42:22 +0000

    [project @ API for vibrating N times.]

--- src/openmoko_vibrator.erl
+++ src/openmoko_vibrator.erl
@@ -1,12 +1,23 @@
 -module(openmoko_vibrator).
 
--export([vibrate/0]).
+-export([vibrate/0, vibrate/1]).
 
 -define(VIBRATOR_CONTROL_FILE, "/sys/class/leds/gta01:vibrator/brightness").
 
+-define(ON_DURATION, 500).
+-define(OFF_DURATION, 500).
+
 vibrate() ->
-    spawn(fun () ->
-		  ok = file:write_file(?VIBRATOR_CONTROL_FILE, list_to_binary("255")),
-		  timer:sleep(500),
-		  ok = file:write_file(?VIBRATOR_CONTROL_FILE, list_to_binary("0"))
-	  end).
+    vibrate(1).
+
+vibrate(N) ->
+    spawn(fun () -> vibrate_n(N) end).
+
+vibrate_n(0) ->
+    ok;
+vibrate_n(N) ->
+    ok = file:write_file(?VIBRATOR_CONTROL_FILE, list_to_binary("255")),
+    timer:sleep(?ON_DURATION),
+    ok = file:write_file(?VIBRATOR_CONTROL_FILE, list_to_binary("0")),
+    timer:sleep(?OFF_DURATION),
+    vibrate_n(N - 1).
